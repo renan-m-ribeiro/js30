b41a269cefa0e88748d0aa389f7c9c701261acd3
Renan Ribeiro <renan.mendes013@gmail.com>
2022-07-14 11:27:55 -0300

    Day 19 (#19)

---
+++ webcam-fun/scripts.js
@@ -0,0 +1,55 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video:true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream)
+            video.play()
+        })
+        .cath(error => console.error(error))
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth
+    const height = video.videoHeight
+    canvas.width = width
+    canvas.height = height
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+        let pixels = ctx.getImagesData(0, 0, width, height)
+        pixels = redEffect(pixels)
+        ctx.putImageData(pixels, 0, 0)
+    },16)
+}
+
+function takePhoto() {
+    snap.currentTime = 0
+    snap.play()
+
+    const data = canvas.toDataURL('image/jpeg')
+    const link = document.createElement('a')
+    link.href = data
+    link.setAttribute('download', 'handsome')
+    link.innerHTML = `<img src='${data}' alt='Handsome man' />`
+    link.textContent = 'Donwnload Image'
+    strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100
+        pixels.data[i + 1] = pixels.data[i + 1] - 50
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5
+    }
+    return pixels
+}
+
+getVideo()
+
+video.addEventListener('canPlay', paintToCanvas)
